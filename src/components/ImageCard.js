c5c58b7620bc72b4573a820321b8b1401c087e12
Rowland Adimoha <rowlives@gmail.com>
2020-10-23 12:31:43 +0100

    completed React ref with grid css for images display

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -3,13 +3,24 @@ import React, { Component } from "react";
 class ImageCard extends Component {
   constructor(props) {
     super(props);
+    this.state = { spans: 0 };
 
-    this.imageRef = React.createRef;
+    this.imageRef = React.createRef();
   }
+  componentDidMount() {
+    this.imageRef.current.addEventListener("load", this.setSpans);
+  }
+  setSpans = () => {
+    const height = this.imageRef.current.ClientHeight;
+
+    const spans = Math.ceil(height / 10);
+
+    this.setState({ spans });
+  };
   render() {
     const { description, urls } = this.props.image;
     return (
-      <div>
+      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
         <img ref={this.imageRef} src={urls.regular} alt={description} />
       </div>
     );
